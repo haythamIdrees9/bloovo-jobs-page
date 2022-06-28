a018818e4d5e32fc29a31e9339652f969dd3e754
haythamIdrees9 <haythamidrees9@gmail.com>
2022-06-28 22:56:32 +0300

    QP-0001 implement the models

---
+++ src/app/job-model.ts
@@ -0,0 +1,32 @@
+// TODO: add all list countries and cities, connect job city array type to selected job city 
+export enum CounterEnum {
+  PALESTINE = 'Palestine',
+  JORDAN = 'Jordan',
+  LEBANON = 'Lebanon',
+  SAUDI_ARABIA = 'Saudi Arabia',
+}
+
+export enum SectorEnum {
+    SALE = 'Sale',
+    AIRLINES = 'Airlines',
+    ADMINISTRATION = 'Administration',
+    COMPUTER_SOFTWARE = 'Computer Software',
+  }
+  
+export const  citiesList:Readonly<{[CounterEnum.PALESTINE]:ReadonlyArray<string>, [CounterEnum.JORDAN]:ReadonlyArray<string>,
+                        [CounterEnum.LEBANON]:ReadonlyArray<string>,[CounterEnum.SAUDI_ARABIA]:ReadonlyArray<string>}> = {
+      [CounterEnum.PALESTINE] : ["Jerusalem","Hebron","Bethlehem","Ramallah"],
+      [CounterEnum.JORDAN] : ["Amman","Zarqa","Irbid","Russeifa"],
+      [CounterEnum.LEBANON] : ["Beirut","Zahle","Tripoli","Sidon"],
+      [CounterEnum.SAUDI_ARABIA] : ["Abha","Ad-Dilam","Al-Abwa","Al Artaweeiyah"]
+}
+
+export type job = {
+  id: number;
+  userId: number;
+  title: string;
+  sector: SectorEnum;
+  country: CounterEnum;
+  city: string;
+  description: string;
+};
